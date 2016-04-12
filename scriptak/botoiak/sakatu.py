3c179da5883c811f11d7db71092da85d2c7162f1
Manex Agirrezabal <manex.agirrezabal@gmail.com>
2016-04-12 18:56:09 +0000

    Botoiak kudeatzeko funtzioa, nabigatzailea pantaila osoan jartzeko scripta eta terminaletik pdf bat inprimatzeko scripta gehituta.

---
+++ scriptak/botoiak/sakatu.py
@@ -0,0 +1,97 @@
+import RPi.GPIO as GPIO ## Import library that lets you control the Pi's GPIO pins
+from time import sleep ## Import time for delays
+
+
+#http://stackoverflow.com/questions/5714072/simulate-keystroke-in-linux-with-python
+from subprocess import Popen, PIPE
+
+
+up_sequence = '''keydown Up
+keyup Up
+'''
+
+down_sequence = '''keydown Down
+keyup Down
+'''
+
+left_sequence = '''keydown Left
+keyup Left
+'''
+
+right_sequence = '''keydown Right
+keyup Right
+'''
+
+on_sequence = '''keydown a
+keyup a
+'''
+
+at_sequence = '''keydown b
+keyup b
+'''
+
+def keypress(sequence):
+    p = Popen(['xte'], stdin=PIPE)
+    p.communicate(input=sequence)
+
+gorapz=37
+beherapz=36
+ezkerpz=32
+eskuinpz=22
+onartupz=11
+atzerapz=16
+
+#GPIO.cleanup()
+GPIO.setwarnings(True) ## Disables messages about GPIO pins already being in use
+GPIO.setmode(GPIO.BOARD) ## Indicates which pin numbering configuration to use
+
+GPIO.setup(gorapz, GPIO.IN) ## Tells it that pin 16 (button) will be giving input
+GPIO.setup(beherapz, GPIO.IN) ## Tells it that pin 16 (button) will be giving input
+GPIO.setup(ezkerpz, GPIO.IN) ## Tells it that pin 16 (button) will be giving input
+GPIO.setup(eskuinpz, GPIO.IN) ## Tells it that pin 16 (button) will be giving input
+GPIO.setup(onartupz, GPIO.IN) ## Tells it that pin 16 (button) will be giving input
+GPIO.setup(atzerapz, GPIO.IN) ## Tells it that pin 16 (button) will be giving input
+
+k=0
+while True:
+  if GPIO.input(gorapz)==1:
+    keypress(up_sequence)
+    sleep(0.25)
+    k=k+1
+    print "GORA"+str(k)
+  if GPIO.input(beherapz)==1:
+    keypress(down_sequence)
+    sleep(0.25)
+    k=k+1
+    print "BEHERA"+str(k)
+    print k
+
+  if GPIO.input(ezkerpz)==1:
+    keypress(left_sequence)
+    sleep(0.25)
+    k=k+1
+    print "EZKERRERA"+str(k)
+    print k
+
+  if GPIO.input(eskuinpz)==1:
+    keypress(right_sequence)
+    sleep(0.25)
+    k=k+1
+    print "ESKUINERA"+str(k)
+    print k
+
+  if GPIO.input(onartupz)==1:
+    keypress(on_sequence)
+    sleep(0.25)
+    k=k+1
+    print "ONARTU"+str(k)
+    print k
+
+  if GPIO.input(atzerapz)==1:
+    keypress(at_sequence)
+    sleep(0.25)
+    k=k+1
+    print "ATZERA"+str(k)
+    print k
+
+GPIO.cleanup()
---
+++ scriptak/fullscreen.sh
@@ -0,0 +1,9 @@
+
+#http://raspberrypi.stackexchange.com/questions/30093/epiphany-browser-in-full-screen-mode
+
+sudo pkill python
+sudo -u pi epiphany-browser -a --profile ~/.config ~/hiztegimakina/web/hasiera.html --display=:0 &
+sudo python botoiak/sakatu.py &
+sleep 5s;
+xte "key F11" -x:0
+
