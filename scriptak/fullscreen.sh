f627e208c49fbdf66c76e20b25095803a30bc0ef
manexagirrezabal <manex.agirrezabal@gmail.com>
2016-04-20 03:19:34 +0000

    Iceweasel nabigatzailea jarrita

--- scriptak/fullscreen.sh
+++ scriptak/fullscreen.sh
@@ -2,7 +2,8 @@
 #http://raspberrypi.stackexchange.com/questions/30093/epiphany-browser-in-full-screen-mode
 
 sudo pkill python
-sudo -u pi epiphany-browser -a --profile ~/.config "http://localhost/hiztegimakina/web/hizkuntzak.html" --display=:0 &
+#sudo -u pi epiphany-browser -a --profile ~/.config "http://localhost/hiztegimakina/web/hizkuntzak.html" --display=:0 &
+iceweasel "http://localhost/hiztegimakina/web/hizkuntzak.html" --display=:0 &
 sudo python botoiak/sakatu.py &
 sleep 5s;
 xte "key F11" -x:0
